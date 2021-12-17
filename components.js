f3bf0074cf59fbaeece53601f7799fb02c7e0de5
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-12-17 20:20:20 +0530

    Update components.js

--- components.js
+++ components.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent("car", {
     },
     init: function () {
         this.el.setAttribute("gltf-model", this.data.modelRef);
-        const position = { x: 5, y: 2, z: 4.5 };
+        const position = { x: 5.3, y: 2, z: 4.7 };
         const rotation = { x: 0, y: -100, z: 0 };
         const scale = {x: 5.9, y: 5.9, z: 5.9};
         this.el.setAttribute("position", position);
@@ -26,6 +26,6 @@ AFRAME.registerComponent("base", {
             radius: this.data.radius,
             height: this.data.height
         });
-        this.el.setAttribute("material", { color: "#1769aa" });
+        this.el.setAttribute("material", { color: "#B87333" });
     },
 });
